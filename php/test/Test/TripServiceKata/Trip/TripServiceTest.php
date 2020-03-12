5b04bf0a196d0b5e3c55d8f2a441393571de6146
Blues <holyblue@interpay.com.tw>
2020-03-12 09:12:49 +0800

    add test case testShouldReturnTripsWhenLoggedUserIsFriend

--- php/test/Test/TripServiceKata/Trip/TripServiceTest.php
+++ php/test/Test/TripServiceKata/Trip/TripServiceTest.php
@@ -3,6 +3,7 @@ namespace Test\TripServiceKata\Trip;
 
 use PHPUnit\Framework\TestCase;
 use TripServiceKata\Exception\UserNotLoggedInException;
+use TripServiceKata\Trip\Trip;
 use TripServiceKata\Trip\TripService;
 use TripServiceKata\User\User;
 use TripServiceKata\User\UserSession;
@@ -34,6 +35,14 @@ class TripServiceTest extends TestCase
         $trips = $this->tripService->getTripsByUser($notfriend);
         $this->assertEquals([], $trips, 'get no trips when not friend');
     }
+
+    public function testShouldReturnTripsWhenLoggedUserIsFriend()
+    {
+        $this->tripService = new FriendTripsService();
+        $friend = new User('friend');
+        $trips = $this->tripService->getTripsByUser($friend);
+        $this->assertNotEmpty($trips);
+    }
 }
 
 class NoLoggedTripService extends TripService
@@ -54,5 +63,24 @@ class NotFriendTripsService extends TripService
     protected function getLoggedUser()
     {
         return new User('loggedUser');
+    }
+}
+
+class FriendTripsService extends TripService
+{
+    protected function isFriend()
+    {
+        return true;
+    }
+
+    protected function getLoggedUser()
+    {
+        return new User('loggedUser');
+    }
+
+    protected function getTrips(\TripServiceKata\User\User $user)
+    {
+        $trips[] = new Trip();
+        return $trips;
     }
 }
